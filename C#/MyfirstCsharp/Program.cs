f33e1ecd6bd3fe14ace9a4b7b714fb39a4f6a95a
Prenga Albert <prengaa@anteacement.com>
2022-07-06 08:30:00 +0200

    I just started C# assignments

---
+++ C#/MyfirstCsharp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int mosha = 18;
+
+if (mosha == 18){
+    Console.WriteLine("Ju tashme mund te pini alkol");
+}
+else if (mosha < 18){
+    Console.WriteLine("Jeni shume i vogel per te pire");
+}
+else{
+    Console.WriteLine("Ju jeni i rritur keshtu qe mund te pini alkol");
+}
+
+Console.WriteLine("Hello World");
