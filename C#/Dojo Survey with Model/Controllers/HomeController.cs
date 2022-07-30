85bf1379cd8c8a24946096fcdad9203fcc2a0dae
Prenga Albert <prengaa@anteacement.com>
2022-07-30 21:03:03 +0200

    Survey with model finished

---
+++ C#/Dojo Survey with Model/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Dojo_Survey_with_Model.Models;
+
+namespace Dojo_Survey_with_Model.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    // [HttpPost("survey")]
+    // public IActionResult Submission(Survey yourSurvey)
+    // {
+    // // Handle your form submission here
+    //     return("index", );
+    // }
+
+    
+    [HttpGet("Add")]
+
+    public IActionResult Add()
+    {
+        return View("Add");
+    }
+    [HttpPost("Create")] 
+     public IActionResult Create(Survey Legend)
+    {   
+        return View("show", Legend);
+ 
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Pr()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ C#/Dojo Survey with Model/Models/Survey.cs
@@ -0,0 +1,13 @@
+
+
+namespace Dojo_Survey_with_Model.Models{
+
+public class Survey
+{
+   public string Name {get; set;} = null!;
+    public string Location {get;set;} = null!;
+   public string Language {get;set;} = null!;
+    public string Comment {get;set;} = null!;
+ 
+}
+}
